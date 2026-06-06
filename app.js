44109cd99e1a6826532ca64cb6999edd14c1c582
Gayna Chawla <gaynachawla@gmail.com>
2026-06-06 23:27:58 +0530

    logic

---
+++ app.js
@@ -0,0 +1,23 @@
+'use strict'
+const numberbox = document.getElementById("numberbox");
+const slider = document.getElementById("slider");
+const progressBar = document.getElementById("progress-bar")
+const playButton = document.getElementById('play-button');
+const pauseButton = document.getElementById("pause-button");
+
+const queen = '<i class="fas fa-chess-queen" style="color:#000"></i>';
+
+let n, speed, tempSpeed, q, Board = 0;
+// Board = 0;
+
+// Creating array for all the possible arrangements of the N-Queen
+let array = [0, 2, 1, 1, 3, 11, 5, 41, 93];
+
+// Used to store the state of the boards;
+let pos = {};
+// let position = {};
+
+
+// Setting the slider value onSlide
+speed = (100 - slider.value) * 10;
+tempSpeed = speed;
